4814967ca540152317a63c64c9b867f353b6043c
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-07-21 16:50:48 -0600

    Create restart-pz-server.sh

---
+++ restart-pz-server.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if ! rcon -a 10.0.0.74:27015 -p 1234 | grep -q "(0)"; then
+    echo "players connected"
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 5 minutes. Get to a safe place!"'
+    sleep 240
+
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 1 minute. Get to a safe place!"'
+    sleep 30
+
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server restarting in 30 seconds!"'
+    sleep 15
+
+    rcon -a 10.0.0.74:27015 -p 1234 'save'
+    sleep 15
+
+    rcon -a 10.0.0.74:27015 -p 1234 'quit'
+    sleep 15
+
+    ssh root@10.0.0.74 'systemctl restart pz-server'
+else
+    echo "no players connected"
+    rcon -a 10.0.0.74:27015 -p 1234 'save'
+    sleep 15
+
+    rcon -a 10.0.0.74:27015 -p 1234 'quit'
+    sleep 15
+
+    ssh root@10.0.0.74 'systemctl restart pz-server'
+fi
