8bd1f83cd9a19fbb963b1ca137af9366c324c788
pr0gsn0b <77224754+pr0gsn0b@users.noreply.github.com>
2025-07-22 06:56:11 -0600

    Update restart-pz-server.sh

--- restart-pz-server.sh
+++ restart-pz-server.sh
@@ -3,16 +3,28 @@
 if ! rcon -a 10.0.0.74:27015 -p 1234 'players' | grep -q "(0)"; then
     echo "players connected"
     rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 5 minutes. Get to a safe place!"'
-    sleep 240
+    sleep 60
 
-    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 1 minute. Get to a safe place!"'
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 4 minutes. Get to a safe place!"'
+    echo "4min"
+    sleep 60
+
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 3 minutes. Get to a safe place!"'
+    echo "3min"
+    sleep 60
+
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 2 minutes. Get to a safe place!"'
+    echo "2min"
+    sleep 60
+
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 1 minutes. Get to a safe place!"'
+    echo "1min"
     sleep 30
 
-    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server restarting in 30 seconds!"'
-    sleep 15
-
+    rcon -a 10.0.0.74:27015 -p 1234 'servermsg "Server will restart in 30 seconds!"'
     rcon -a 10.0.0.74:27015 -p 1234 'save'
-    sleep 15
+    echo "30sec"
+    sleep 30
 
     rcon -a 10.0.0.74:27015 -p 1234 'quit'
     sleep 15
